b916c699e082f43b2a6bc9d44a9f23bae9615975
Michal Jewczuk <michal.jewczuk@outlook.com>
2019-08-13 08:56:55 +0200

    Created AppConfig file; moved structure.json to main folder

---
+++ src/main/java/nodomain/jsongenerator/config/AppConfig.java
@@ -0,0 +1,8 @@
+package nodomain.jsongenerator.config;
+
+public class AppConfig {
+
+	public static final String CONFIGURATION_FILE = "structure.json";
+	public static final String DEFAULT_OUTPUT_NAME = "data_";
+	
+}
