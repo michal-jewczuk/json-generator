e4327745c70954411b32820948b1b58e4520ff21
Michal Jewczuk <michal.jewczuk@outlook.com>
2019-08-14 14:18:24 +0200

    Implemented NumberFragmentGenerator to generate numbers

--- src/main/java/nodomain/jsongenerator/generator/NumberFragmentGenerator.java
+++ src/main/java/nodomain/jsongenerator/generator/NumberFragmentGenerator.java
@@ -1,6 +1,10 @@
 package nodomain.jsongenerator.generator;
 
+import java.util.Random;
+import java.util.stream.LongStream;
+
 import nodomain.jsongenerator.data.DataOptions;
+import nodomain.jsongenerator.data.NumberDataOptions;
 
 public enum NumberFragmentGenerator implements FragmentGenerator {
 	
@@ -8,9 +12,17 @@ public enum NumberFragmentGenerator implements FragmentGenerator {
 
 	public StringBuilder generateFragment(String name, DataOptions options) {
 		StringBuilder fragment = generateBegining(name);
-		fragment.append(System.currentTimeMillis());
+		fragment.append(generateNumber(options));
 
 		return fragment;
+	}
+	
+	private long generateNumber(DataOptions options) {
+		NumberDataOptions opt = (NumberDataOptions) options;
+		Random rand = new Random();
+		LongStream str = rand.longs(1, opt.getNumberMin(), opt.getNumberMax());
+		
+		return str.sum();
 	}
 
 }
