948ea5b57be83726e623da175209f5b67e4aeb00
Michal Jewczuk <michal.jewczuk@outlook.com>
2019-08-21 08:37:04 +0200

    Created new RuntimeException JsonParsingException

--- src/main/java/nodomain/jsongenerator/config/AppConfig.java
+++ src/main/java/nodomain/jsongenerator/config/AppConfig.java
@@ -6,4 +6,6 @@ public class AppConfig {
 	public static final String DEFAULT_OUTPUT_NAME = "data";
 	public static final String STRING_SYMBOLS = "abcdefghijklmnopqrstuvwyz";
 	
+	public static final String ERROR_PARSING = "Error parsing element: ";
+	
 }
---
+++ src/main/java/nodomain/jsongenerator/exceptions/JsonParsingException.java
@@ -0,0 +1,11 @@
+package nodomain.jsongenerator.exceptions;
+
+public class JsonParsingException extends RuntimeException {
+
+	private static final long serialVersionUID = -2790910919426902819L;
+	
+	public JsonParsingException(String message) {
+		super(message);
+	}
+
+}
